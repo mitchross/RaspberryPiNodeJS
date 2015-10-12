e6a174a896d678fb5c4bbb6c2bc4c1278f538b5a
pascal0828 <pascal0828@gmail.com>
2015-10-11 22:50:26 -0400

    Fix

--- routes/garage.js
+++ routes/garage.js
@@ -1,11 +1,9 @@
-var express = require('express');
-var router = express.Router();
 var garage = require('../controllers/garage');
 
-router.post("/api/garage/openclose" , function (req , res )
-{
-  res.send(200);
-  garage.startGarageDoorLoop();
-});
-
-module.exports = router;
+module.exports = function(app){
+	app.post("/api/garage/openclose" , function (req , res )
+	{
+	  res.send(200);
+	  garage.startGarageDoorLoop();
+	});
+}
